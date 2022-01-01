07bcf41efcf1916598bfa9a3b83ec08f737ec843
salt-dev <loukasmalias@hotmail.com>
2022-01-01 12:49:42 +0200

    basic styling added on welcome component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -10,45 +10,8 @@ import { UserCard } from "./components/UserCard";
 const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:4000';
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_URL);
-// const userSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:4000/loukas');
 
 const App = () => {
-  // const [items, setItems] = useState<Array<Item>>([]);
-
-  // socket.on("items", (itemList: Array<Item>) => {
-  //   setItems(itemList);
-  // });
-
-  // const toggleComplete = (e: React.MouseEvent<HTMLDivElement>) => {
-  //   const selectedItem = items.filter(todo => todo.title === e.currentTarget.id)[0];
-  //   socket.emit('changeStatus', selectedItem);
-  // };
-
-  // const toggleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   const selectedItem: string | null = e.currentTarget.id;
-  //   socket.emit('removeItem', selectedItem);
-  // };
-
-  // const addItem = (title: string, desc: string) => {
-  //   socket.emit('addItem', {title, desc});
-    
-  //   // newItem.trim() !== '' && setItems([...items, {text: newItem, complete: false }])
-  // };
-
-  // const handleToggleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   if (e.currentTarget.textContent === 'Completed') {
-  //     socket.emit("filterCompleted");
-  //     socket.on('returnFilteredData', (filteredData: any) => {
-  //       setItems(filteredData);
-  //     });
-  //   };
-  //   if (e.currentTarget.textContent === 'Pending') {
-  //     socket.emit("filterPending");
-  //     socket.on('returnFilteredData', (filteredData: any) => {
-  //       setItems(filteredData);
-  //     });
-  //   };
-  // };
 
   return(
     <React.Fragment>
--- Client/src/components/Welcome.tsx
+++ Client/src/components/Welcome.tsx
@@ -2,14 +2,14 @@ import React, { useState, ChangeEvent } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Welcome.css';
 
-interface WelcomeProps {
-  // item: Item;
-  // toggleComplete: ToggleComplete;
-  // toggleEdit: ToggleEdit;
-  // toggleRemove: ToggleRemove;
-}
+// interface WelcomeProps {
+//   // item: Item;
+//   // toggleComplete: ToggleComplete;
+//   // toggleEdit: ToggleEdit;
+//   // toggleRemove: ToggleRemove;
+// }
 
-export const Welcome: React.FC<WelcomeProps> = () => {
+export const Welcome: React.FC = () => {
   const [url, setUrl] = useState<string>('');
 
   const navigate = useNavigate();
@@ -27,11 +27,13 @@ export const Welcome: React.FC<WelcomeProps> = () => {
   }
 
   return (
-    <div>
-      <label>Join an existing project</label>
-      <input type='text' placeholder="copy/paste your id" onChange={handleChange}></input>
-      <button type='submit' onClick={handleExistingSubmit}>Go</button>
-      <button type='submit' onClick={handleNewSubmit}>Create New Project</button>
+    <div className="welcome__main">
+      <label className="main__title">Join an existing project</label>
+      <input type='text' className="main__input" placeholder="enter your project name" onChange={handleChange}></input>
+      <button type='submit' className="main__button" onClick={handleExistingSubmit}>Go</button>
+      <label className="main__title">Create a new project</label>
+      <input type='text' className="main__input" placeholder="name your project" onChange={handleChange}></input>
+      <button type='submit' className="main__button" onClick={handleNewSubmit}>Create</button>
     </div>
   );
 };
