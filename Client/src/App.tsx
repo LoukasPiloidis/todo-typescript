969c7b39e12460f9fc6a1dd6bdcafd01053838e7
salt-dev <loukasmalias@hotmail.com>
2022-01-05 17:51:52 +0100

    about page implemented

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,6 +5,7 @@ import './styles/App.css';
 import { UserCard } from "./components/UserCard";
 import { Logout } from "./components/Logout";
 import Burger from "./components/Burger";
+import { About } from "./components/About";
 
 const App = () => {
   const [user, setUser] = useState<string | null>(null);
@@ -19,6 +20,8 @@ const App = () => {
           <Route path='/' element={<Welcome />}>
           </Route>
           <Route path="/logout" element={<Logout getUser={getUser} />}>
+          </Route>
+          <Route path="/about" element={<About />}>
           </Route>
           <Route path="/:id" element={<UserCard getUser={getUser} />}>
           </Route>
---
+++ Client/src/components/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/About.css';
+
+export const About: React.FC = () => {
+
+  return (
+    <div className="about-main">
+      <h2 className="about-title">About this app</h2>
+      <p>This app was developed with Typescript, React, and socket.io. It is deployed on Heroku, as part of an assignment for ubiquity's screening process.</p>
+      <br />
+      <p>The sole developer is Loukas Piloidis.</p>
+      <br />
+      <p>You can find his GitHub page <a href="https://github.com/LoukasPiloidis">here</a>, his linkedIn profile <a href="https://www.linkedin.com/in/loukaspiloidis/">here</a>, and his facebook profile <a href="https://www.facebook.com/loukas.piloidis">here</a>.</p>
+    </div>
+  );
+};
