50b0e76f388bb3f40c4bbe3fefb07cc9a35dd645
salt-dev <loukasmalias@hotmail.com>
2022-01-07 19:47:28 +0100

    updated global v

--- Server/db/mongo.ts
+++ Server/db/mongo.ts
@@ -8,9 +8,9 @@ const uri = `mongodb+srv://Utes:${process.env.MONGO_PASS}@cluster0.qt1uh.mongodb
 const client = new MongoClient(uri);
 await client.connect();
 
-const db = client.db(process.env.DB_NAME || process.env.DB_NAME_TEST);
-export const itemsCollection = db.collection(process.env.DB_COLLECTION || process.env.DB_COLLECTION_TEST);
-export const usersCollection = db.collection(process.env.DB_COLLECTION2 || process.env.DB_COLLECTION2_TEST);
+const db = client.db('Todo-typescript');
+export const itemsCollection = db.collection('Items');
+export const usersCollection = db.collection('Users');
 
 export const createItem = async (item: Item) => {
   await itemsCollection.insertOne(item);
--- Server/dist/db/mongo.js
+++ Server/dist/db/mongo.js
@@ -5,9 +5,9 @@ config();
 const uri = `mongodb+srv://Utes:${process.env.MONGO_PASS}@cluster0.qt1uh.mongodb.net/Todo-typescript?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 await client.connect();
-const db = client.db(process.env.DB_NAME || process.env.DB_NAME_TEST);
-export const itemsCollection = db.collection(process.env.DB_COLLECTION || process.env.DB_COLLECTION_TEST);
-export const usersCollection = db.collection(process.env.DB_COLLECTION2 || process.env.DB_COLLECTION2_TEST);
+const db = client.db('Todo-typescript');
+export const itemsCollection = db.collection('Items');
+export const usersCollection = db.collection('Users');
 export const createItem = async (item) => {
     await itemsCollection.insertOne(item);
 };
